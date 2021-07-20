bbaeea6f247bf1e92ad679c2022412f335338db6
zarkykai <892688602@qq.com>
2021-07-20 23:54:03 +0800

    控制台界面实现

--- josephus/src/josephus.py
+++ josephus/src/josephus.py
@@ -1,21 +1,22 @@
-#find josephus out of n people when kill one out of every q people
-import Reader       #文件读取方法以及Person类存储在自定义的Reader模块中
+# find josephus out of n people when kill one out of every q people
+import josephus.src.Reader       # 文件读取方法存储在自定义的Reader模块中
 
 ZIPPATH = 'ADcarry.zip'
 ZIP_TARGET_FILE = 'ADcarry/ADcarry.csv'
 CSVPATH = 'ADcarry2/ADcarry.csv'
 EXCELPATH = 'ADcarry2/ADcarry.xlsx'
 
-STEP=3
-begin_person='卡莎'
+STEP = 3
+begin_person = '卡莎'
 
-#函数实现约瑟夫环
-def get_death_order(input_list, step, start_person ):
+
+# 函数实现约瑟夫环
+def get_death_order(input_list, step, start_person):
     death_list = []
     list_copy = input_list.copy()
     rem = len(list_copy)
 
-################输入检查，输入的起始位置是否在列表中
+# ###############输入检查，输入的起始位置是否在列表中
     start_point = -1
     for x in list_copy:
         if x.name == start_person:
@@ -23,7 +24,7 @@ def get_death_order(input_list, step, start_person ):
             print('\033[0;32;40m\t start person founded \033[0m')
 
     if start_point == -1:
-        start_point = 0             #若列表中没有找到输入的起始值，则将起始值置为0
+        start_point = 0             # 若列表中没有找到输入的起始值，则将起始值置为0
         print('\033[0;31;40m\t start person not founded, the start person has been set to the first one \033[0m')
 #################
     assert start_point >= 0
@@ -31,59 +32,62 @@ def get_death_order(input_list, step, start_person ):
         assert step != 0
         '''判断输入的步长的正负-----------------------'''
         if step > 0:
-            out_point = (start_point -1 + step )%rem
+            start_point = (start_point - 1 + step) % rem
 
         if step < 0:
-            out_point = (start_point + 1 + step) % rem
+            start_point = (start_point + 1 + step) % rem
         '''-----------------------------------------------'''
 
-        death_list.append(list_copy[out_point])
-        del list_copy[out_point]
+        death_list.append(list_copy[start_point])
+        del list_copy[start_point]
 
-        start_point = out_point
-        rem = rem -1
+        rem = rem - 1
 
     return death_list
 
-#使用迭代器类实现约瑟夫环
-class JosephusRing():
 
-    def __init__(self, ipt_list = [], step = 0, start_person = ''):
-        assert  step >= 0
+# 使用迭代器类实现约瑟夫环
+class JosephusRing:
+
+    def __init__(self, ipt_list, step=0, start_person=''):
+        assert step >= 0
         assert type(ipt_list) == list
-        assert  type(start_person) == str
+        assert type(start_person) == str
 
         self.__list = ipt_list.copy()
         self.__step = step
         self.__start_person = start_person
         self.death_list = []
+        self.__pointer = self.set_pointer()
 
-        ################检查起始位置是否在列表中
-        self.__pointer = -1
+    def set_pointer(self):
+        judge = -1
         for x in self.__list:
             if x.name == self.__start_person:
-                self.__pointer = self.__list.index(x)       #若在列表中找到,则将pointer设置为起始值的索引
+                self.__pointer = self.__list.index(x)       # 若在列表中找到,则将pointer设置为起始值的索引
+                judge = 0
                 print('\033[0;32;40m\t start person founded \033[0m')
+                break
 
-        if self.__pointer == -1:
+        if judge == -1:
             self.__pointer = 0  # 若列表中没有找到输入的起始值，则将pointer置为0
             print('\033[0;31;40m\t start person not founded, the start person has been set to the first one \033[0m')
-        #################
+        return self.__pointer
+
     def __iter__(self):
         return self
 
     def __next__(self):
-        # if self.__rem > 0:
         if len(self.__list) > 0:
-            self.__pointer = (self.__pointer - 1 + self.__step) % len(self.__list)      #迭代公式，对索引进行运算
+            self.__pointer = (self.__pointer - 1 + self.__step) % len(self.__list)      # 迭代公式，对索引进行运算
 
-            self.death_list.append(self.__list[self.__pointer])     #
+            self.death_list.append(self.__list[self.__pointer])     # 把索引指定值加入death_list中
             del self.__list[self.__pointer]
 
-
         else:
-            raise  StopIteration
-        return self.death_list[len(self.death_list)-1]   #返回死亡名单中的最新一个对象
+            raise StopIteration
+        return self.death_list[-1]   # 返回死亡名单中的最新一个对象
+
 
 if __name__ == '__main__':
     '''调用不同的Reader进行文件读取'''
@@ -93,8 +97,8 @@ if __name__ == '__main__':
     list_ = obj.read()
 
     '''文件读取结果打印'''
-    for x in list_:
-        print("name:",x.name,"id:",x.id)
+    for i in list_:
+        print("name:", i.name, "id:", i.id)
 
     '''josephus排序并打印结果(使用函数）'''
     # death_list = get_death_order(list,STEP,begin_person)
@@ -103,7 +107,7 @@ if __name__ == '__main__':
     #     print("name:", y.name, "id:", y.id)
 
     '''josephus排序并打印结果（使用迭代器类）'''
-    JS_interable_obj = JosephusRing(list_,STEP,begin_person) #建立可迭代对象，自身就是迭代器
+    JS_interable_obj = JosephusRing(list_, STEP, begin_person)    # 建立可迭代对象，自身就是迭代器
     print('\033[0;32;40m\t the josephus death order is: \033[0m')
     for i in JS_interable_obj:
-        print("name:",i.name,"id:",i.id)
+        print("name:", i.name, "id:", i.id)
---
+++ josephus/ui/ConsoleUI.py
@@ -0,0 +1,127 @@
+from josephus.src.josephus import JosephusRing
+from josephus.src.Reader import ExcelReader, CsvReader
+from josephus.src.Person import Person
+from PyQt5.QtWidgets import *
+import os
+# import tkinter as tk
+# from tkinter import filedialog
+#
+# root = tk.Tk()
+# root.withdraw()
+
+
+class JosephusUI():
+    def __init__(self):
+        # super(JosephusUI, self).__init__()
+        self.__file_name = ''
+        self.__list = []
+        self.__step = 0
+        self.__begin_person = ''
+        self.cwd = os.getcwd()
+
+        while 1:
+            self.main_process()
+
+            bool_ = input('是否退出？若否则回到程序开始（y/n）')
+            if bool_ == 'y':
+                break
+            elif bool_ == 'n':
+                pass
+            else:
+                print('error input')
+
+    def read_file_by_dialog(self):
+        # obj = QFileDialog()       #想要使用文件对话框选择并打开文件,但除了使用按钮连接的方式，似乎无法直接使用文件对话框
+        # obj.setFileMode(QFileDialog.AnyFile)
+        # obj.setFilter('(*.csv *.xls *.xlsx )')
+        # if obj.exec():
+        #     self.__file_name = obj.selectedFiles()
+
+        # self.__file_name, filetype = obj.getOpenFileName(obj, "选取文件", self.cwd, "All Files (*);;Text Files (*.txt)")
+
+        # self.__file_name = filedialog.askopenfilename()
+
+        print(os.listdir('../test/ADcarry2'))
+        print(self.cwd)
+        file_name = input('请选择要打开的文件（请直接将上面的文件名复制下来使用）：')
+        self.__file_name = '../test/ADcarry2' + '/' + file_name
+        # 此处应添加assert限制所选文件类型
+
+        print(file_name)
+        if file_name.endswith('csv'):
+            self.__list = CsvReader(self.__file_name).read()
+        elif file_name.endswith('xls') | self.__file_name.endswith('xlsx'):
+            self.__list = ExcelReader(self.__file_name).read()
+        else:
+            print('unsupported file type')
+            self.__list = []
+
+        return None
+
+    def get_list(self):
+        while 1:
+            bool_ = input('是否要选择文件以读取名单（y/n）:')
+            if bool_ == 'y':
+                self.read_file_by_dialog()
+                break
+            elif bool_ == 'n':
+                while 1:
+                    bool__ = input('是否手动输入信息（y/n）')
+                    if bool__ == 'n':
+                        break
+
+                    elif bool__ == 'y':
+                        name_ = input('请输入姓名：')
+                        id_ = input('请输入学号：')
+                        self.__list.append(Person(name_, id_))
+                        self.show_list()
+
+                    else:
+                        print('error input')
+
+                break
+
+            else:
+                print('error input')
+
+        self.show_list()
+
+    def show_list(self):
+        print('-----------------------------')
+        print('list:\n')
+        for i in self.__list:
+            print("name:", i.name, "id:", i.id)
+        print('-----------------------------')
+
+    def set_step_and_start_person(self):
+        self.show_list()
+        self.__step = int(input('设置步长为：'))
+        self.__begin_person = input('设置第一个报数人：')
+
+    def main_process(self):
+        '''输入list的获取'''
+        while 1:
+            self.get_list()
+            bool_ = input('名单输入是否完成？若是则启动算法进行重新排序（y/n）')
+            if bool_ == 'y':
+                break
+
+        '''设置步长与起始人'''
+        self.set_step_and_start_person()
+
+        '''运行约瑟夫环算法'''
+        interable_obj = JosephusRing(self.__list, self.__step, self.__begin_person)
+        '''打印结果'''
+        print('-----------------------------')
+        print('\033[0;32;40m\t the josephus death order is: \033[0m')
+        for i in interable_obj:
+            print("name:", i.name, "id:", i.id)
+        print('-----------------------------')
+
+    def save_file_to(self):
+        pass        #csv与xls文件的写操作
+
+
+if __name__ == '__main__':
+    JSui = JosephusUI()
+
