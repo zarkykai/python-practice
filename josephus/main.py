b8aebfe6773568c8d60fe292c7e49b4bfc00dc17
zarkykai <892688602@qq.com>
2021-07-23 13:42:27 +0800

    接口放至最外层，并修改了bug使其能够运行

--- josephus/main.py
+++ josephus/main.py
@@ -1,24 +1,30 @@
-from PyQt5.QtWidgets import *
-import ConsoleUI
-from JsDialogUI import JsDUI
-from josephus.src.josephus import JosephusRing
-from josephus.src.Reader import ExcelReader, CsvReader
-from josephus.src.Person import Person
+import sys
+import os
+
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+
+from ui.ConsoleUI import JosephusUI
+from ui.JsDialogUI import JsDUI
+from src.josephus import JosephusRing
+from src.Reader import ExcelReader, CsvReader
+from src.Person import Person
 
 
-class JosephusUI(QDialog,JsDUI): # 此处类的命名应再加考虑
+class JSUI(QDialog, JsDUI): # 此处类的命名应再加考虑
     def __init__(self):
-        super(JosephusUI,self).__init__()
+        super(JSUI, self).__init__()
         self.setupUi(self)
-        # 设置信号连接
-        self.choose_file_pushButton.clicked.connect(self.slot_input_from_file)
-        self.manual_input_pushButton.clicked.connect(self.slot_add_person)
-        self.start_process_Button.clicked.connect(self.slot_start_process)
+
         self.__list = []
         self.__file_name = ''
         self.step = 0
         self.start_person = ''
         self.death_list = []
+
+        # 设置信号连接
+        self.choose_file_pushButton.clicked.connect(self.slot_input_from_file)
+        self.manual_input_pushButton.clicked.connect(self.slot_add_person)
+        self.start_process_Button.clicked.connect(self.slot_start_process)
 
     def slot_add_person(self):
         name_ = self.name_lineEdit.text()
@@ -26,20 +32,20 @@ class JosephusUI(QDialog,JsDUI): # 此处类的命名应再加考虑
         # 检测输入内容是否合法，是否为空等
         self.__list.append(Person(name_, id_))
 
-        for i in self.__list:
-            self.ipt_list_textBrowser.append(self.__list[i])
+        self.ipt_list_textBrowser.append('name:'+self.__list[-1].name+'   '+'id:'+self.__list[-1].id)
 
     def slot_input_from_file(self):
-        self.__file_name = QFileDialog.getOpenFileName(self, "选取文件", os.getcwd())
+        self.__file_name, _ = QFileDialog.getOpenFileName(self, "选取文件", os.getcwd())
 
-        if self.__file_name.endswith('csv'):
+        if self.__file_name.endswith('csv'):        # 将读取出来的列表拼接到list上
             self.__list = CsvReader(self.__file_name).read()
         elif self.__file_name.endswith('xls') | self.__file_name.endswith('xlsx'):
             self.__list = ExcelReader(self.__file_name).read()
         else:
             print('unsupported file type')
+            self.ipt_list_textBrowser.clear()
         for i in self.__list:
-            self.ipt_list_textBrowser.append(self.__list[i])
+            self.ipt_list_textBrowser.append('name:'+i.name+'   '+'id:'+i.id)
 
     def slot_start_process(self):
         self.step = int(self.step_lineEdit.text())
@@ -49,18 +55,19 @@ class JosephusUI(QDialog,JsDUI): # 此处类的命名应再加考虑
         for i in interable_obj:
             self.death_list.append(i)
 
-        for i in self.__list:
-            self.death_list_textBrowser.append(self.death_list[i])
+        self.death_list_textBrowser.clear()
+        for i in self.death_list:
+            self.death_list_textBrowser.append('name:'+i.name+'   '+'id:'+i.id)
 
 if __name__ == '__main__':
     print('请选择使用控制台界面or图形界面')
     bool_ = input('1、控制台界面\n2、图形界面\n')
     if bool_ == '1':
-        ui = ConsoleUI.JosephusUI()
+        ui = ConsoleUI.JSUI()
     elif bool_ == '2':
         app = QApplication([])
         # 初始化
-        myWin = JosephusUI()
+        myWin = JSUI()
         # 将窗口控件显示在屏幕上
         myWin.show()
         # 程序运行，sys.exit方法确保程序完整退出。
